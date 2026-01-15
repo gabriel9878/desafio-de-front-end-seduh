eddad933b7c43d57252e4a7c6bc46ca5a45a0a2c
Gabriel Souza <53787151+gabriel9878@users.noreply.github.com>
2026-01-15 16:29:39 -0300

    Ajusta basePath/assetPrefix para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
   // Se um dia usar next/image, isso garante que funcione no export estático:
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? "/desafio-de-front-end-seduh" : "",
+  assetPrefix: isProd ? "/desafio-de-front-end-seduh" : "",
 };
 
 export default nextConfig;
